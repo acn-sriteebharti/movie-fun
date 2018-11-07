a3d1e805f7522ed1bc1caf9cb96b4ce187d22fba
kalyani <kalyani.s.kanuganti@accenture.com>
2018-11-07 16:10:34 -0500

    remove persistence to filesystems

---
+++ src/main/java/org/superbiz/moviefun/blobstore/Blob.java
@@ -0,0 +1,22 @@
+package org.superbiz.moviefun.blobstore;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import java.io.InputStream;
+
+public class Blob {
+    public final String name;
+    public final InputStream inputStream;
+    public final String contentType;
+
+    public Blob(String name, InputStream inputStream, String contentType) {
+        this.name = name;
+        this.inputStream = inputStream;
+        this.contentType = contentType;
+    }
+
+}
